f450ebfdd7a469f89877b24642d45aac7128cf55
THẮNG NGÔ QUANG <thang.ngothang2210@hcmut.edu.vn>
2025-04-26 17:14:21 +0000

    feat(tasks): add GET /tasks and POST /tasks

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+
+tasks = []
+@app.get("/tasks")
+def list_tasks():
+    return tasks
+
+@app.post("/tasks")
+def create_task(task: dict):
+    tasks.append(task)
+    return task
